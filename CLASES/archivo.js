26930ade8dfe6e8ab8269efe18fdeaf4b17f0f0f
Samuel Cano <samuelcanoe@gmail.com>
2025-03-24 02:56:02 -0500

    Add files via upload

---
+++ CLASES/archivo.js
@@ -0,0 +1,14 @@
+var nombre = "Pedro" ;
+var Nombre = "Anny" ;
+var _nombre = "Tomas";
+var NOMBRE = "Elizabeth";
+var $_nombre = "Antonio"; 
+
+let saludo = "hola"; 
+let Saludo = "que tal?" ;
+let _saludo = "como te va?" ;
+let SALUDO = "encantado";
+let $_saludo = "buenos dias";
+
+document.write(SALUDO);
+
